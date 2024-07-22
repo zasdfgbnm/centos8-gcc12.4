c89aa52491ddbee49195c80b9fbb927a94e87fa1
Gao, Xiang <qasdfgtyuiop@gmail.com>
2024-07-22 15:03:23 -0700

    Update install-dependencies.sh

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-dnf install -y yum-utils rpm-build
+dnf install -y yum-utils rpm-build wget
 dnf install -y binutils-devel dblatex dejagnu docbook-style-xsl doxygen elfutils-devel elfutils-libelf-devel flex gcc gcc-c++ gcc-toolset-12-gdb gdb gettext glibc-static gmp-devel graphviz libmpc-devel libzstd-devel mpfr-devel python3-sphinx rpm-devel scl-utils-build sharutils texinfo texinfo-tex texlive-collection-latex
 dnf install -y glibc-devel.i686 libgcc.i686
 
